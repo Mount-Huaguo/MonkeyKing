0d67241b2372232972c008d8d279d104670d2358
GitHub Action <action@github.com>
2021-05-04 08:52:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/heramerom/monkeyking/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/heramerom/monkeyking/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.heramerom.monkeyking.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heramerom.monkeyking.MyBundle
 
 class MyApplicationService {
 
