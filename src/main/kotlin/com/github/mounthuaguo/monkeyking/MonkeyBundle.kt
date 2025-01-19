a0e193d038f6f847f18b8d2c9b5c6f8352328501
dawenxi <dawenxi.tech@gmail.com>
2025-01-19 10:19:19 +0800

    Update to remove deprecated methods

--- src/main/kotlin/com/github/mounthuaguo/monkeyking/MonkeyBundle.kt
+++ src/main/kotlin/com/github/mounthuaguo/monkeyking/MonkeyBundle.kt
@@ -10,18 +10,17 @@ private const val BUNDLE = "messages.MKBundle"
 
 object MonkeyBundle : AbstractBundle(BUNDLE) {
 
-  @Suppress("SpreadOperator")
-  @JvmStatic
-  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-    getMessage(key, *params)
+    @Suppress("SpreadOperator")
+    @JvmStatic
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
 
-  @Suppress("SpreadOperator")
-  @JvmStatic
-  fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-    getLazyMessage(key, *params)
+    @Suppress("SpreadOperator")
+    @JvmStatic
+    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+        getLazyMessage(key, *params)
 }
 
 object MonkeyIcons {
-  val LUA = IconLoader.getIcon("/asserts/lua.svg")
-  val JS = IconLoader.getIcon("/asserts/js.svg")
+    val LUA = IconLoader.getIcon("/asserts/lua.svg", MonkeyIcons.javaClass)
+    val JS = IconLoader.getIcon("/asserts/js.svg", MonkeyIcons.javaClass)
 }
